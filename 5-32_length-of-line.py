07c2b661f9c2cf99cf1e4dea9a556565772738f0
yamn15 <yamatooooo15@gmail.com>
2020-11-11 10:44:51 +0900

    commit_5-32-done_20201111

---
+++ 5-32_length-of-line.py
@@ -0,0 +1,32 @@
+print('**********************************************************************')
+
+# 長い１文を単純に改行するだけではエラーが返ってくる
+# s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' + 
+#       'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+# print(s)
+
+print('**********************************************************************')
+
+# 長い１文を改行したい場合は、バックスラッシュ\を打てばいい
+s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' \
+       'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+print(s)
+
+x = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1+ 1 \
+    + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+print(x)
+
+print('**********************************************************************')
+
+# パレンティスの丸かっこでも改行対応可能
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+       'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
+
+x = (1 + 1 + 1 + 1 + 1 + 1 + 1 + 1+ 1 
+    + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1)
+print(x)
+
+
+
+## pythonでは1行の長さが80文字以上であれば、次の行に開業するべきという考え方がある
