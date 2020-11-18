8f6925b6199f5578e0e62ed0eee14c36cd6f57c6
yamn15 <yamatooooo15@gmail.com>
2020-11-19 00:10:19 +0900

    commit_5-39-done_20201118

--- 5-38_if_Discriminant-not.py
+++ 5-38_if_Discriminant-not.py
@@ -10,11 +10,28 @@ print(type(is_empty))
 
 print('**********************************************************************')
 
+# pythonでは等式を使用して変数がNoneであるか否かを判別することは推奨されていない
 if is_empty == None:
     print('None!!!')
 
+# pythonでは [is XXX]を使用して変数がNoneであるか否かを判別するほうが好ましい
+if is_empty is None:
+    print('None!!!')
 
+if is_empty is not None:
+    print('None!!!')
 
+print('**********************************************************************')
 
+# isは[is_empty]のような変数に入っているオブジェクトがNoneかどうかを判定する際によく使われる
+## 以下簡単な例
 
+## 値を1として、真同士なのでTrueと出力される
+print(type(1 == True))
+print(1 == True)
 
+## オブジェクト同士が同じものであれば、真。そうでなければ偽
+print(type(1 is True))
+print(1 is True)
+print(True is True)
+print(None is None)
