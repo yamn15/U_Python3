85517d5329d2cb43c7f019979cf393e4362bf0b3
yamn15 <yamato.kawamura.p@gmail.com>
2021-09-23 17:19:14 +0900

    commit_section6=20210923

---
+++ 6-70_import-astarisc.py
@@ -0,0 +1,11 @@
+
+
+# from lesson_package_6_68.talk_6_69 import human
+# from lesson_package_6_68.talk_6_69 import animal_6_70
+from lesson_package_6_68.talk_6_69 import *
+
+# print(animal_6_70.sing())
+# print(animal_6_70.cry())
+
+## セクション6は複数ファイルを作るのが面倒くさくなり、動画視聴のみでいったん終了。
+## パッケージやモジュールを記載する際は、他ファイルから参照される可能性を考え、__name__ , __main__を使用した関数を作成して使用する必要があることのみ覚えておくこと！
---
+++ lesson_package_6_68/talk_6_69/animal_6_70.py
@@ -0,0 +1,13 @@
+
+from lesson_package_6_68.tools_6_69 import utils2
+
+def sing():
+    return 'qwertyuiose45tgsing'
+
+def cry():
+    return utils2.say_twice('asdr56789okjhnbcry')
+
+
+## print('********************************************************************')
+## print('(1)')
+### ↑モジュール呼び出す際に、なぜか8,9行目も出力されてしまう。仕組みを要復習
